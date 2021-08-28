665a7975ca83a466dcf85d21a08930268ccac47c
ttm1309 <89680872+ttm1309@users.noreply.github.com>
2021-08-28 15:11:18 +0700

    Create adm.js

---
+++ modules/commands/adm.js
@@ -0,0 +1,16 @@
+module.exports.config = {
+	name: "adm",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Nguyễn Minh Quân",
+	description: "Thông tin người điều hành bot",
+	commandCategory: "info",
+	cooldowns: 1
+};
+
+module.exports.run = ({ event, api }) => api.sendMessage(`Thông tin người điều hành bot:
+Facebook: Trần Trọng Mạnh 
+Username: trongmanh2004
+Giới tính: Nam
+Facebook : https://www.facebook.com/ttm130`, event.threadID, event.messageID);
+
